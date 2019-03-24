dbb621123d41d22bfc65310d9a52764de9a82e50
dmarch <damamo82@gmail.com>
2019-03-24 23:46:11 +0000

    readme and data paths

--- R/data_paths.R
+++ R/data_paths.R
@@ -1,14 +1,40 @@
 #----------------------------------------------------------------
 # data_paths.R   Define paths to datasets
 #----------------------------------------------------------------
-# Date: 2018/11/13
-# Author: David March
-# Project: Tortugas oceanografas
-#----------------------------------------------------------------
+# This file provide the paths to raw datasets and generated products
+# Note that default are Windows file paths. Adapt them to your OS accordingly.
+
+
+## Set absolute paths of the three main data folders
+raw_dir <- "E:/abigoosv2/data/raw"  # raw data folder
+temp_dir <- "E:/abigoosv2/data/temp"  # temporary data folder
+out_dir <- "E:/abigoosv2/data/out" # output data folder
+
+## Set relative paths of the raw datasets
+
 
 
 ## Data folder
 hd <- "H:/data/"
+
+# Raw data ----------------------------------------------------------------
+
+## Project data
+
+
+## External data
+
+
+# * Bathymetry
+# * Argo profiles from GDAC
+# * MEOP-CTD database
+# * OBIS-SEAMAP
+# * IUCN spatial data
+# * Birdlife
+# * SWOT
+# * SeaVox Salt and Fresh Water Body Gazetter (v16 2015)
+# * Land mask, Natural Earth
+
 
 
 # External data ----------------

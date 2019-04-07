462591438d7207d7c444f91bc030ea748a734494
dmarch <damamo82@gmail.com>
2019-04-07 19:08:02 +0100

    rasterized pinniped telemetry

--- R/05_telemetry/03_pinniped2raster.R
+++ R/05_telemetry/03_pinniped2raster.R
@@ -20,16 +20,84 @@ mask <- raster(temp_mask)
 outfile <- paste0(temp_dir, "/meop_profiles.csv")
 profdf <- read.csv(outfile)
 
-
 ## Create raster in longlat to use as base for rasterization
 r <- raster(xmn=-180, xmx=180, ymn=-90, ymx=90, crs=CRS("+proj=longlat +ellps=WGS84"),
             resolution=c(1, 1), vals=NA)
 
 
-
-
-
-
 #--------------------------------------------------------
 # 1. Rasterize presence of telemetry records per species
 #--------------------------------------------------------
+
+## Prepare pre-processing table
+match <- dplyr::select(match, taxonid, scientific_name)
+df <- filter(match, scientific_name %in% unique(profdf$scientific_name))
+
+## Rasterize MEOP data per species
+pinniped <- stack()  # create stack
+
+for (i in 1:nrow(df)){
+  
+  ispp <- df$scientific_name[i]
+  ispid <- as.character(df$taxonid[i])
+  print(ispp)
+  
+  ## Subset data
+  data <- filter(profdf, scientific_name == ispp)
+  
+  ## Convert to spatial class
+  coordinates(data) <- ~lon+lat
+  proj4string(data) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+  
+  # transform coordinates to mollweide
+  data <- spTransform(data, CRSobj = PROJ)
+  
+  ## rasterize distribution data
+  rdata <- rasterize(coordinates(data), mask, fun = "count") # count number of profiles
+  rdata[rdata==0]<-NA
+  
+  ## filter out land cells
+  rdata <- rdata * mask
+  
+  ## convert to binary
+  rdata <- rdata/rdata
+  
+  ## save into stack
+  names(rdata) <- ispid
+  pinniped <- stack(pinniped, rdata)
+}
+
+# save raster with telemetry presence data per species
+outfile <- paste0(temp_dir, "/", "meopPinniped.grd")
+writeRaster(pinniped, filename=outfile, bandorder='BIL', overwrite=TRUE)
+
+
+
+#--------------------------------------------------------
+# 2. Calculate density of profiles per km2 for all spp
+#--------------------------------------------------------
+
+
+## Use complete dataset
+data <- profdf
+
+## Convert to spatial class
+coordinates(data) <- ~lon+lat
+proj4string(data) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+
+# transform coordinates to mollweide
+data <- spTransform(data, CRSobj = PROJ)
+
+## rasterize distribution data
+rdata <- rasterize(coordinates(data), mask, fun = "count") # count number of profiles
+rdata[rdata==0]<-NA
+
+## filter out land cells
+rdata <- rdata * mask
+
+## Calculate density: number of profiles per km2
+rdata  <- rdata  / ((res(rdata )[1]/1000)*(res(rdata )[2]/1000))  # divide by km2
+
+# save raster with telemetry density observations
+outfile <- paste0(temp_dir, "/", "meopPinDens.grd")
+writeRaster(rdata, filename=outfile, bandorder='BIL', overwrite=TRUE)
--- R/05_telemetry/04_telemetry_plots.R
+++ R/05_telemetry/04_telemetry_plots.R
@@ -16,13 +16,18 @@ box <- bb(xmin = -180, xmax = 180, ymin = -90, ymax = 90, crs=PROJ)
 ## Import data
 land.prj <- readOGR(temp_dir, temp_land)  # landmask
 
+## Import sea turtle density of number of records
 turtle_dens <- paste0(temp_dir, "/", "obisTurtlesDens.grd")
 chelo <- raster(turtle_dens)
+
+## Import pinnipeds density of number of profiles
+pin_dens <- paste0(temp_dir, "/", "meopPinDens.grd")
+pin <- raster(pin_dens)
+
 
 #-------------------------------------------------------------------------
 # Fig 1. Density maps of sea turtle telemetry number of records
 #-------------------------------------------------------------------------
-# Plot of number of species per taxa.
 
 # Create plot
 p <- plotraster(r = log10(chelo), land = land.prj, box = box,
@@ -30,4 +35,17 @@ p <- plotraster(r = log10(chelo), land = land.prj, box = box,
 
 # Save as png file
 p_png <- paste(fig_dir,"telemetry","turtle_telemetry_density.png", sep="/")
+ggsave(p_png, p, width=25, height=14, units="cm", dpi=300)
+
+
+#-------------------------------------------------------------------------
+# Fig 2. Density maps of pinniped telemetry number of profiles
+#-------------------------------------------------------------------------
+
+# Create plot
+p <- plotraster(r = log10(pin), land = land.prj, box = box,
+                legendTitle = expression(log[10]~(profiles / km^2)))
+
+# Save as png file
+p_png <- paste(fig_dir,"telemetry","pinniped_telemetry_density.png", sep="/")
 ggsave(p_png, p, width=25, height=14, units="cm", dpi=300)
